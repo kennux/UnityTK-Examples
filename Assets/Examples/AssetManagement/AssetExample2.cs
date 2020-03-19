b5b881ae081e2017a2aeaa8858ca3e2661adca62
Kenneth Ellersdorfer <kennux@ymail.com>
2020-03-19 20:34:54 +0100

    Initial import

---
+++ Assets/Examples/AssetManagement/AssetExample2.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityTK.AssetManagement;
+
+public class AssetExample2 : MonoBehaviour
+{
+    public string testStr;
+}
